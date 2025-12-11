a2d5ff6f0d9c0450ce78280cba69a8d08a4a4148
elnatanorato <elnatan.honorato@gmail.com>
2025-12-11 19:07:22 -0300

    fix: fix layout login

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -15,17 +15,19 @@ export default function LoginPage() {
     {}
   );
   return (
-    <div className="login-container">
-      <div className="login-content">
-        <h1 className="login-title">Login</h1>
-        <form action={loginFormAction} className="login-form">
-          {loginState.errors && (
-            <span className="login-error">{loginState.errors}</span>
-          )}
-          <Input label="username" name="username" />
-          <Input label="senha" type="password" name="password" />
-          <Button disabled={loginPending}>Login</Button>
-        </form>
+    <div style={{ flex: 1, backgroundColor: "#f9f9f9" }}>
+      <div className="login-container">
+        <div className="login-content">
+          <h1 className="login-title">Login</h1>
+          <form action={loginFormAction} className="login-form">
+            {loginState.errors && (
+              <span className="login-error">{loginState.errors}</span>
+            )}
+            <Input label="username" name="username" />
+            <Input label="senha" type="password" name="password" />
+            <Button disabled={loginPending}>Login</Button>
+          </form>
+        </div>
       </div>
     </div>
   );
